487f1ec0f35e06a9d8e29070e680b9cf8a17771e
Max Johansen <maxjohansen@berkeley.edu>
2015-08-05 16:03:56 -0600

    fix

--- collections/users/Code/lib/UsersCollection.js
+++ collections/users/Code/lib/UsersCollection.js
@@ -20,18 +20,10 @@ var childSchema = new SimpleSchema({
     optional:true,
     public:true
   },
-<<<<<<< HEAD
   gender:{
     type:String,
     allowedValues:["male","female"]
   }
-=======
-    gender: {
-        type: String,
-        optional:true,
-        public:true
-    },
->>>>>>> origin/displayChildren
 });
 
 Schema.UserProfile = new SimpleSchema({
